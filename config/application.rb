a309ceeaf8312c9ae34d3aee2d6fdd257a19d1d1
Jason Soo <wwwjscom@gmail.com>
2013-01-20 04:09:41 -0500

    This work?

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Repper
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Heroku: https://devcenter.heroku.com/articles/rails3x-asset-pipeline-cedar
+    config.assets.initialize_on_precompile = false
   end
 end
