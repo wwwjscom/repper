2f3ae6a6e587b9e5cce054098f5567d04fa2b71c
Jason Soo <wwwjscom@gmail.com>
2013-01-20 03:17:09 -0500

    init

---
+++ app/models/workout.rb
@@ -0,0 +1,3 @@
+class Workout < ActiveRecord::Base
+  attr_accessible :user_id
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Repper::Application.config.secret_token = 'b9e36a46d55be91239cf46dab95db2e9d338cd7865a53f2f49209132a74646c138dc892c175295e584e2f0e16ade3b9b0bcaed6337e18be73b9323140c2a7a8e'
