3479e64a003f98123e5c8ef209d99efa8030a2bb
Jason Soo <wwwjscom@gmail.com>
2013-01-25 00:36:20 -0500

    FIXED: Percentage output of single rep max

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -2,32 +2,40 @@ class Evaluation < ActiveRecord::Base
   attr_accessible :bicep_reps, :bicep_weight, :tricep_reps, :tricep_weight, :back_reps, :back_weight, :lower_back_reps, :lower_back_weight, :chest_reps, :chest_weight, :crunch_reps, :legs_reps, :legs_weight, :shoulder_reps, :shoulder_weight, :user_id
   
   belongs_to :user
-  
-  def light_weight(workout)
-    (get_reps_and_weight_for(workout) || 0) *0.7
+
+  def _60_weight(workout)
+    (one_rep_max(workout) || 0) *0.60
   end
   
-  def med_weight(workout)
-    (get_reps_and_weight_for(workout) || 0) *0.75
+  def _65_weight(workout)
+    (one_rep_max(workout) || 0) *0.65
+  end  
+  
+  def _70_weight(workout)
+    (one_rep_max(workout) || 0) *0.7
   end
   
-  def heavy_weight(workout)
-    (get_reps_and_weight_for(workout) || 0) *0.80
+  def _75_weight(workout)
+    (one_rep_max(workout) || 0) *0.75
   end
   
-  def one_rep_max(reps, weight)
+  def _80_weight(workout)
+    (one_rep_max(workout) || 0) *0.80
+  end
+  
+  def calc_one_rep_max(reps, weight)
     (((reps.to_f/30)+1)*weight.to_f)
   end
   
-  def get_reps_and_weight_for(workout)
+  def one_rep_max(workout)
     case workout
-    when :chest then one_rep_max(chest_reps, chest_weight)
-    when :bicep then one_rep_max(bicep_reps, bicep_weight)
-    when :tricep then one_rep_max(tricep_reps, tricep_weight)
-    when :crunch then one_rep_max(crunch_reps, 10) # FIXME: How do we calc this?
-    when :shoulder then one_rep_max(shoulder_reps, shoulder_weight)
-    when :back then one_rep_max(back_reps, back_weight)
-    when :lower_back then one_rep_max(lower_back_reps, lower_back_weight)
+    when :chest then calc_one_rep_max(chest_reps, chest_weight)
+    when :bicep then calc_one_rep_max(bicep_reps, bicep_weight)
+    when :tricep then calc_one_rep_max(tricep_reps, tricep_weight)
+    when :crunch then calc_one_rep_max(crunch_reps, 10) # FIXME: How do we calc this?
+    when :shoulder then calc_one_rep_max(shoulder_reps, shoulder_weight)
+    when :back then calc_one_rep_max(back_reps, back_weight)
+    when :lower_back then calc_one_rep_max(lower_back_reps, lower_back_weight)
     end
   end
 end
