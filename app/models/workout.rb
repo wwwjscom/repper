062498d31ba6cdca7c7cc1587ea199d9136fd2fc
Jason Soo <wwwjscom@gmail.com>
2013-01-25 17:43:02 -0500

    FIXED: Workout generation bugs

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -12,6 +12,7 @@ class Workout < ActiveRecord::Base
     
     reps_and_weight = {target_groups[0] => [], target_groups[1] => []}
 
+
     logger.debug "tg: #{target_groups.inspect}"
     reps_and_weight[target_groups[0]] = get_reps_and_weight(user, target_groups[0])
     reps_and_weight[target_groups[1]] = get_reps_and_weight(user, target_groups[1])
@@ -84,23 +85,47 @@ class Workout < ActiveRecord::Base
       
       # Don't workout the same muscle groups as last time
       last_primary_group = user.workouts.last.muscle_group_1_id
+      last_secondary_group = user.workouts.last.muscle_group_2_id
+      logger.debug "Last prim group: #{last_primary_group}"
       last_group_index = muscle_groups.index(MuscleGroup.find(last_primary_group))
       
-      target_groups << get_next_muscle_group(last_group_index, muscle_groups)
-      if muscle_groups.size > 1
-        target_groups << get_next_muscle_group(last_group_index, muscle_groups)
+      if muscle_groups.size == 1
+        target_groups = get_next_muscle_group(last_group_index, muscle_groups, 1)
+      else
+        # We add +1 to the last_group_index here so that we can skip over the
+        # previous 2 muscle groups that were used.  That is, since there are
+        # >1 muscle groups selected by the user, we automatically selected a
+        # primary and secondary group for the previous work.  Therefore, 
+        # since last_group_index is the index of the last primary group, go
+        # ahead and skip over the secondary group too, which is +1 of the
+        # primary group in the muscle groups array.
+        last_secondary_group_index = muscle_groups.index(MuscleGroup.find(last_secondary_group))
+        target_groups = get_next_muscle_group(last_secondary_group_index, muscle_groups, 2)
       end
     end    
     return target_groups
   end
 
   # Iterates over the muscle groups selecting the next ones in line
-  def self.get_next_muscle_group(curr_index, muscle_groups)
-    if (curr_index + 1) > muscle_groups.size
-      return muscle_groups[0]
+  def self.get_next_muscle_group(curr_index, muscle_groups, count)
+    next_muscle_groups = []
+    logger.debug "curr_index: #{curr_index}"
+    logger.debug "count: #{count}"
+    logger.debug "muscle_groups.size: #{muscle_groups.size}"
+    if (curr_index + 1) >= muscle_groups.size
+      logger.debug "branch 1"
+      next_muscle_groups << muscle_groups[0]
+      next_muscle_groups << muscle_groups[1] if count == 2
+    elsif (curr_index + 2) >= muscle_groups.size
+      logger.debug "branch 2"
+      next_muscle_groups << muscle_groups.last
+      next_muscle_groups << muscle_groups[0] if count == 2
     else
-      return muscle_groups[curr_index + 1]
+      logger.debug "branch 3"
+      next_muscle_groups << muscle_groups[curr_index+1]
+      next_muscle_groups << muscle_groups[curr_index+2] if count == 2
     end
     
+    next_muscle_groups
   end
 end
