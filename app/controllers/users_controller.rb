90d2be0a1511c707396769db0c006b6eb27c9eb4
Jason Soo <wwwjscom@gmail.com>
2013-01-25 03:33:20 -0500

    FIXED: Some lava code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,22 +18,19 @@ class UsersController < ApplicationController
   
   def edit
     @user = current_user
-    @muscle_groups = @user.muscle_group_ids
   end
   
   def update
     @user = User.find(current_user)
+    # Initialize
     params[:user][:muscle_group_ids] ||= []
-   active_groups = params[:user][:muscle_group_ids]
-   
-
+    # Backup for later assignment
+    active_groups = params[:user][:muscle_group_ids]
+    # Reset so mass attr update doesn't bomb
     params[:user][:muscle_group_ids] = []
     
     
     if @user.update_attributes(params[:user])      
-      # Reset old muscle groups
-      @user.muscle_groups = []
-
       # Setup new muscle groups
       active_groups.each do |id, checked|
         @user.muscle_groups << MuscleGroup.find_by_name(id) if checked == "1"      
