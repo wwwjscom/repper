953ebe54a640e2b509d77915962abbb6d37bbfa5
Jason Soo <wwwjscom@gmail.com>
2013-01-27 02:10:54 -0500

    ADDED: New user input fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  attr_accessible :email, :password, :password_confirmation, :muscle_group_ids, :goal
+  attr_accessible :email, :password, :password_confirmation, :muscle_group_ids, :goal, :age, :sex, :experience
 
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
