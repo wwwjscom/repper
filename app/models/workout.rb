1f302ad06db616309d67ebb0f8446b9098ea1108
Jason Soo <wwwjscom@gmail.com>
2013-02-05 00:57:06 -0500

    FIXED: A few minor bugs

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -113,7 +113,7 @@ class Workout < ActiveRecord::Base
     # Workout the abs?
     if construct_an_abs_workout
       logger.debug "Constructing abs workout"
-      generate_abs_workout(user).each do |exercise_and_reps_hash|
+      WorkoutUnitAb.generate_abs_workout(user).each do |exercise_and_reps_hash|
         logger.debug "Adding #{exercise_and_reps_hash.inspect}"
         WorkoutUnitAb.create(
           :user_id     => user.id,
@@ -375,60 +375,5 @@ class Workout < ActiveRecord::Base
     end
     
     next_muscle_groups
-  end
-  
-  # Generate the abs portion of the workout.
-  # Parameters
-  #   user - The user obj of the current user
-  #
-  # Returns
-  #   A array of hashes, where each hash has an Exercise obj
-  #     and an associated number of reps.
-  def self.generate_abs_workout(user)
-    exercises          = pick_abs_exercises
-    exercises_and_reps = pick_abs_reps(exercises, user)
-    exercises_and_reps
-  end
-  
-  # Randomly selects _count_ number of abs exercises
-  # and returns them in an array of Exercise objects.
-  #
-  # Parameters
-  #   count - number of abs exercises to return
-  #
-  # Returns
-  #   An array of Exercises objects.
-  def self.pick_abs_exercises(count = 4)
-    chosen_exercises = []
-    abs_exercises = MuscleGroup.find_by_name("abs").exercises
-    count.times do
-      rand_index = rand(abs_exercises.size)
-      chosen_exercises << abs_exercises[rand_index]
-    end    
-    chosen_exercises
-  end
-  
-  # Decides how many reps of each abs exercises should be
-  # done for the user.  This should be based on their previous
-  # ability to complete the given number of abs reps.
-  #
-  # Parameters
-  #   exercises - An array of abs Exercises objects
-  #   user - The user who owns the workout
-  #
-  # Returns
-  #   A array of hashes, where each hash has an Exercise obj
-  #     and an associated number of reps.
-  def self.pick_abs_reps(exercises, user)
-    exercises_and_reps = []
-    # TODO: Add the logic here to intelligently generate abs reps
-    exercises.each do |e|
-      exercises_and_reps << { 
-        :exercise => e,
-        :reps     => 12 
-      }
-    end
-    exercises_and_reps
-  end
-  
+  end  
 end
