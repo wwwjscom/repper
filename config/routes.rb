3ac7ecc376a2c795b9a28f0ba35b0ecff5267894
Jason Soo <wwwjscom@gmail.com>
2013-07-20 04:08:35 -0400

    Good _enough_ homepage, for now.

--- config/routes.rb
+++ config/routes.rb
@@ -73,6 +73,7 @@ Repper::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
+  match 'index.html' => 'home#index'
   root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
