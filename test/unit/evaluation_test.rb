a7a26b8697e300a3bc0ac7107532c89f09f36b1e
Jason Soo <wwwjscom@gmail.com>
2013-08-01 17:22:47 -0400

    ADDED: Some basic tests for muscle groups

--- test/unit/evaluation_test.rb
+++ test/unit/evaluation_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class EvaluationTest < ActiveSupport::TestCase
-   test "the truth" do
-     assert true
+   test "one rep max" do
+     e = users(:jason).evaluations.first
+     assert_equal(150, e.one_rep_max(:chest))
+     assert_equal(45, e.one_rep_max(:shoulder))
+     assert_equal(66.66666666666666, e.one_rep_max(:bicep))
+     assert_equal(42.0, e.one_rep_max(:tricep))
+     assert_equal(nil, e.one_rep_max(:legs))
+     assert_equal(133.33333333333331, e.one_rep_max(:back))
+     assert_equal(41.666666666666664, e.one_rep_max(:"lower back"))
+     assert_equal(30.0, e.one_rep_max(:abs))
    end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -5,4 +5,10 @@ class UserTest < ActiveSupport::TestCase
   test "the user basics" do
      assert_equal(users(:jason).id, 1)
    end
+   
+   test "muscle groups" do
+     mg = users(:jason).muscle_groups
+     assert_equal(5, mg.size)
+     assert_equal(muscle_groups(:chest).name, mg[0].name)
+   end
 end
