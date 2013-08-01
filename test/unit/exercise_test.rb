97ef6040f71a66cab267a84d3b15e1bb68b7332a
Jason Soo <wwwjscom@gmail.com>
2013-08-01 17:33:47 -0400

    ADDED: Some exercises tests

--- test/unit/exercise_test.rb
+++ test/unit/exercise_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ExerciseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "weight interval" do
+    assert_equal(10, Exercise.weight_interval(exercises(:row_machine)))
+  end
+  
+  test "rounding to a doable weight" do
+    assert_equal(60, Exercise.round_up_to_doable_weight(exercises(:row_machine), 54.5))
+    assert_equal(60, Exercise.round_up_to_doable_weight(exercises(:row_machine), 50.001))
+    assert_equal(60, Exercise.round_up_to_doable_weight(exercises(:row_machine), 59.99))
+  end
 end
